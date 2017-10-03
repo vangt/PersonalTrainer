198b07a700c858a6829bccb9edc719109214c4ba
vangt <vangt86@gmail.com>
2017-10-03 14:13:51 -0500

    Added HistoryModel to keep track of previous inputed weights.

---
+++ PersonalTrainer/Migrations/201710031849295_Added user name to the required fill in for Registration.cs
@@ -0,0 +1,16 @@
+namespace PersonalTrainer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedusernametotherequiredfillinforRegistration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- PersonalTrainer/Models/AccountViewModels.cs
+++ PersonalTrainer/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace PersonalTrainer.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -68,6 +67,10 @@ namespace PersonalTrainer.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
