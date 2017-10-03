a56e32363057bde28a609fbc55b271eda6e1019e
vangt <vangt86@gmail.com>
2017-10-03 13:36:51 -0500

    Added user variables.

--- PersonalTrainer/Models/IdentityModels.cs
+++ PersonalTrainer/Models/IdentityModels.cs
@@ -9,6 +9,17 @@ namespace PersonalTrainer.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+        public string Age { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
