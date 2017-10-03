0510f5717b2b6b878646b07edb197d0996caec40
vangt <vangt86@gmail.com>
2017-10-03 14:20:44 -0500

    Added a profile information page to input key information.

---
+++ PersonalTrainer/Controllers/ManageAccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using PersonalTrainer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalTrainer.Controllers
+{
+    public class ManageAccountController : Controller
+    {
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+        // GET: ManageAccount
+        public ActionResult Index()
+        {
+            var userName = User.Identity.GetUserName();
+            var user = _context.Users.Where(x => x.UserName == userName).First();
+
+            return View(user);
+        }
+
+        public ActionResult ProfileInfo()
+        {
+            var userName = User.Identity.GetUserName();
+            var user = _context.Users.Where(x => x.UserName == userName).First();
+
+            return View(user);
+        }
+    }
+}
