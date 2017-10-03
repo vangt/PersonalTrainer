339838760963cf294601c8c5d0ff20391be9c6d4
vangt <vangt86@gmail.com>
2017-10-03 11:44:37 -0500

    Add project files.

---
+++ PersonalTrainer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalTrainer.Startup))]
+namespace PersonalTrainer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
